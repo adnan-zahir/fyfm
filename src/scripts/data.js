fa12e3e8d9138b287ad536b95089634363020732
Adnan Zahir <adnanzaidanz@gmail.com>
2021-01-18 01:47:39 +0700

    searchMovies.js function

---
+++ src/scripts/data.js
@@ -0,0 +1,4 @@
+const baseUrl = "https://api.themoviedb.org/3";
+const apiKey = "9b651285bf3fae21dd77597bd249ef3b";
+
+export { baseUrl, apiKey };
--- src/scripts/getMovies.js
+++ src/scripts/getMovies.js
@@ -1,13 +1,36 @@
-const apiKey = "9b651285bf3fae21dd77597bd249ef3b";
-const baseUrl = "https://api.themoviedb.org/3";
-
-const listTitle = document.querySelectorAll(".title2")
+import { baseUrl, apiKey } from "./data.js";
+const listTitle = document.querySelectorAll(".title2");
+const search = document.querySelector("#search");
+const searchBtn = document.querySelector("#submit");
 
 const popularMovies = async () => {
     const res = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}`);
     const data = await res.json();
     renderMovies(data.results);
+    listTitle.forEach(title => title.innerHTML = "Popular");
+}
 
+// const searchMovies = async () => {
+//     const keyword = search.value
+//     const res = await fetch(`${baseUrl}/search/movie?api_key=${apiKey}&query=${keyword}`);
+//     const data = await res.json();
+//     renderMovies(data.results);
+//     listTitle.forEach(title => title.innerHTML = "Popular");
+// }
+
+const searchMovies = () => {
+    const keyword = search.value;
+    fetch(`${baseUrl}/search/movie?api_key=${apiKey}&query=${keyword}`)
+    .then((res) => res.json())
+    .then((data) => {
+        if (data.results.length > 0) {
+            renderMovies(data.results);  
+        } else {
+            alert(`${keyword} tidak ditemukan`)
+        }
+    })
+    .catch((err) => alert(err.status_message));
+    listTitle.forEach(title => title.innerHTML = `Search : ${keyword}`);
 }
 
 const renderMovies = (movies) => {
@@ -23,4 +46,6 @@ const renderMovies = (movies) => {
     });
 }
 
-export { popularMovies }
+searchBtn.addEventListener("click", searchMovies)
+
+export { popularMovies };
